d0bf753c401a1b2388745f30bbfc04ff43b2b32f
vvhappyguy <vvhappyguy@gmail.com>
2018-03-16 17:01:42 +0300

    FTP-Exceptions added.

--- main_theading.py
+++ main_theading.py
@@ -2,23 +2,71 @@
 import threading
 from jsoninfo import JsonInfo
 import ftplib
+import sys
 
 
 class myThread(threading.Thread):
     def __init__(self,threadID):
-        """FTP-connection"""
-        self.ftp = ftplib.FTP(host=parser.host, user=parser.username,
-            passwd=parser.password, acct=parser.port)
-        """Thread Data"""
         self.threadID = threadID
+        """FTP-connection"""
+        try:
+            self.ftp = ftplib.FTP(host=parser.host, acct=parser.port)
+        except TimeoutError as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        except ftplib.error_proto as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        except ftplib.error_reply as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        except ftplib.error_temp as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        else:
+            print("[LOG][{}]: Good Host-address".format(self.threadID))
+        try:
+            self.ftp.login(user=parser.username, passwd=parser.password)
+        except ftplib.error_perm as ftp_error:
+            self.ftp.close()
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        except ftplib.error_proto as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        except ftplib.error_reply as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        except ftplib.error_temp as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+            sys.exit()
+        else:
+            print("[LOG][{}]: Connected successfully".format(self.threadID))
+        
+        
+        """Thread Data"""
         self.in_dir = parser.files[threadID]['file_input']
         self.out_dir = parser.files[threadID]['file_output']
         self.file_exp = parser.files[threadID]['file_extention']
         threading.Thread.__init__(self)
     def run(self):
-        ftp_upload(self.ftp,self.in_dir, self.out_dir,self.file_exp)
-        print("Thread " + str(self.threadID) + " has been uploaded.")
-        self.ftp.close()
+        try:
+            ftp_upload(self.ftp,self.in_dir, self.out_dir,self.file_exp)
+        except ftplib.error_perm as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+        except FileNotFoundError as in_error:
+            print("[LOCAL-ERROR][{}]: {}".format(self.threadID,in_error))
+        except ftplib.error_proto as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+        except ftplib.error_reply as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+        except ftplib.error_temp as ftp_error:
+            print("[FTP-ERROR][{}]: {}".format(self.threadID,ftp_error))
+        else:
+            print("[LOG][{}]: Has been uploaded.".format(self.threadID))
+        finally:
+            self.ftp.close()
+            print("[LOG][{}]: FTP-Connection closed.".format(self.threadID))
 
 def ftp_upload(ftp_obj, in_path, out_path, ftype):
     """
@@ -27,7 +75,7 @@ def ftp_upload(ftp_obj, in_path, out_path, ftype):
     @param in_path: Destination file (from)
     @param out_path: Destination file (to)
     """
-    if ftype == 'TXT':
+    if ftype == 'txt':
         with open(in_path) as fobj:
             ftp_obj.storlines('STOR ' + out_path, fobj)
     else:
@@ -51,10 +99,11 @@ if __name__ == "__main__":
             t.join()
     
     """For checking files on FTP-server"""
-    ftp = ftplib.FTP(host=parser.host, user=parser.username,
-                         passwd=parser.password, acct=parser.port)
-
+   #ftp = ftplib.FTP(host=parser.host, user=parser.username,
+    #                     passwd=parser.password, acct=parser.port)
+"""
     print("\n")
     data = ftp.retrlines('LIST')
     print("\n" + data)
+"""
     
